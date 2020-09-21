e9ab6d67e317f5a843e55b75cec7f165895000ce
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2020-09-21 20:28:03 +0600

    topbar, navbar & footer template add in Base HTML

--- dj_realstate/dj_realstate/settings.py
+++ dj_realstate/dj_realstate/settings.py
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'dj_realstate/static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')   #Copy from STATICFILES_DIRS
